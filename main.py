d82b405fbd8a66063811e91333e378e3a49fb869
Jose Negron <josenj@gmail.com>
2021-06-16 12:41:00 -0400

    Cleaned up comments

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#                  - OpenPLC Python SubModule (PSM) -
-# 
-# PSM is the bridge connecting OpenPLC core to Python programs. PSM allows
-# you to directly interface OpenPLC IO using Python and even write drivers 
-# for expansion boards using just regular Python.
-#
 # PSM API is quite simple and just has a few functions. When writing your
 # own programs, avoid touching on the "__main__" function as this regulates
 # how PSM works on the PLC cycle. You can write your own hardware initialization
@@ -23,11 +17,9 @@
 # output (%QX0.0) is true, PSM will display "QX0.0 is true" on OpenPLC's
 # dashboard. Feel free to reuse this skeleton to write whatever you want.
 
-#import all your libraries here
-import psm
+from psm import psm
 import time
 
-#global variables
 counter = 0
 var_state = False
 
@@ -50,7 +42,6 @@ def update_outputs():
     a = psm.get_var("QX0.0")
     if a == True:
         print("QX0.0 is true")
-
 
 if __name__ == "__main__":
     hardware_init()
